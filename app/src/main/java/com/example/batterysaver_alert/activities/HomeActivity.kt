5c7500c0df3ebf42d4ce36ab5024774624d18b2e
DH.1005.2001 <hoangtranminhduc1005@gmail.com>
2023-08-07 13:28:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/batterysaver_alert/activities/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.batterysaver_alert.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.batterysaver_alert.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
