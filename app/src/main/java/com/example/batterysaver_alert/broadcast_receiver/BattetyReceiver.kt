4ac0743fc9d957d4253b34b3a2cc8d32461c2814
rakadin <hoangtranminhduc1005>
2023-08-09 17:11:07 +0700

    ajust layouts and add battery status notification warning, setting choice

--- app/src/main/java/com/example/batterysaver_alert/broadcast_receiver/BattetyReceiver.kt
+++ app/src/main/java/com/example/batterysaver_alert/broadcast_receiver/BattetyReceiver.kt
@@ -1,21 +1,33 @@
 package com.example.batterysaver_alert.broadcast_receiver
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.media.RingtoneManager
 import android.os.BatteryManager
+import android.os.Build
+import android.text.Html
 import android.text.format.DateUtils
+import android.util.Log
 import android.widget.TextView
+import androidx.core.app.NotificationCompat
 import com.example.batterysaver_alert.R
 import com.example.batterysaver_alert.activities.HomeActivity
 import pl.droidsonroids.gif.GifImageView
 
 class BattetyReceiver : BroadcastReceiver() {
-
+    private val TYPE_BATTERY_LOW = "low"
+    private val TYPE_BATTERY_HOT = "temperature"
+    private val TYPE_BATTERY_LOW_ALARM = "alarm_low"
+    private val TYPE_BATTERY_HOT_ALARM = "temperature"
     override fun onReceive(context: Context, intent: Intent) {
+        val sharedPreferences = context?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
         // This method is called when the BroadcastReceiver is receiving an Intent broadcast.( Dynamic way)
         if(intent?.action == Intent.ACTION_BATTERY_CHANGED)
         {
+
             val chargingStatus = intent.getIntExtra(BatteryManager.EXTRA_STATUS, -1)
             val isCharging = chargingStatus == BatteryManager.BATTERY_STATUS_CHARGING
             val isFull = chargingStatus == BatteryManager.BATTERY_STATUS_FULL
@@ -26,7 +38,9 @@ class BattetyReceiver : BroadcastReceiver() {
             val batteryLevel = (level.toFloat() / scale.toFloat()) * 100.0f
             val temperature = intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, -1) / 10.0f
 
-            // get text ids
+            val batteryManager = context.getSystemService(Context.BATTERY_SERVICE) as BatteryManager
+            val capacity = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)            // get text ids to set text
+
             val percentTextView = (context as? HomeActivity)?.findViewById<TextView>(R.id.percent_in)
             val capacityText = (context as? HomeActivity)?.findViewById<TextView>(R.id.capacity_in)
             val temperatureTextView = (context as? HomeActivity)?.findViewById<TextView>(R.id.temperature_in)
@@ -38,13 +52,13 @@ class BattetyReceiver : BroadcastReceiver() {
             // handle battery information
             temperatureTextView?.text = "$temperature °C" // set temperature
             percentTextView?.text = "$batteryLevel%"// set percent now
-            capacityText?.text = "$scale mah" // set battery capacity
-            if(isCharging){
+            capacityText?.text = "$capacity mah" // set battery capacity
+            if(isCharging){ // if the phone is charging
                 chargeStateTextView?.text = "Charging"
                 lastCharge?.text = "Now"
                 battery_gif?.setImageResource(R.drawable.battery_charge)
             }
-            if(isFull){
+            if(isFull){ // if the battery is fully charge
                 chargeStateTextView?.text = "Full"
                 battery_gif?.setImageResource(R.drawable.battery_full)
             }
@@ -62,6 +76,50 @@ class BattetyReceiver : BroadcastReceiver() {
                 }
             }
 
+            // check if user setting is enable some of this notification below
+            val isNotiTextChecked = sharedPreferences?.getBoolean("noti_text",false)
+            val isNotiSoundChecked = sharedPreferences?.getBoolean("noti_sound",false)
+            val low_limitString = sharedPreferences?.getString("percent_limit","10%")
+            val heat_limitString = sharedPreferences?.getString("heat_limit","40°C")
+
+            //get 10 from 10% string
+            val numeric_percent_Value = low_limitString?.replace("%", "")?.toFloatOrNull()
+            val numeric_heat_Value = heat_limitString?.replace("°C", "")?.toFloatOrNull()
+
+            Log.v("check_sound",isNotiSoundChecked.toString())
+            Log.v("check_text",isNotiTextChecked.toString())
+            Log.v("get_int",numeric_percent_Value.toString())
+            Log.v("get_int",numeric_percent_Value!!.toString())
+
+
+            if(isNotiTextChecked == true){ // if noti on -> check next
+                if(isNotiSoundChecked == false){ // else noti with no sound
+                    // set limit to send notification to user
+                    Log.v("check_text","text_called")
+                    if(batteryLevel <= numeric_percent_Value!!){
+                        // should save database in this
+                        setBatteryNotificationForUser(context,10,TYPE_BATTERY_LOW,temperature)
+                    }
+                    if(temperature >= numeric_heat_Value!!){
+                        setBatteryNotificationForUser(context,10,TYPE_BATTERY_HOT,temperature)
+                    }
+                }
+                else{ // if noti sound checked -> noti with sound
+                    // set limit to send notification with sound to user
+                    Log.v("check_text","sound_called")
+                    if(batteryLevel <= numeric_percent_Value!!){
+                        // should save database in this
+                        setBatteryNotificationForUser(context,10,TYPE_BATTERY_LOW_ALARM,temperature)
+                    }
+                    if(temperature>= numeric_heat_Value!!){
+                        setBatteryNotificationForUser(context,10,TYPE_BATTERY_HOT_ALARM,temperature)
+                    }
+                }
+
+            }
+
+//
+
         }
 
         // when the charger is unplug- static way ( even when the app is not running )
@@ -69,7 +127,6 @@ class BattetyReceiver : BroadcastReceiver() {
             val unplugTimeMillis = System.currentTimeMillis()
 
             // Save the unplug time to shared preferences
-            val sharedPreferences = context?.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
             sharedPreferences?.edit()?.putLong("unplug_time", unplugTimeMillis)?.apply()
 
             val chargeLastTextView = (context as? HomeActivity)?.findViewById<TextView>(R.id.chargeLast_in)
@@ -86,11 +143,59 @@ class BattetyReceiver : BroadcastReceiver() {
                 // The phone has not been unplugged since the app was installed or preferences were cleared
             }
         }
+    }
+    // create notification for user
+    private fun setBatteryNotificationForUser(context: Context?,limit : Int, type :String, temperature: Float){
+        val notificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-//        // when the charger is plug in
-//        if(intent?.action == Intent.ACTION_POWER_CONNECTED){
-//            val chargeLastTextView = (context as? HomeActivity)?.findViewById<TextView>(R.id.chargeLast_in)
-//            chargeLastTextView?.text = "Now"
-//        }
+        // Create a notification channel (required for Android 8.0 and above)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "battery_channel",
+                "Battery Channel",
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notificationBuilder = NotificationCompat.Builder(context, "battery_channel")
+        // Create the battery low notification
+        if( type == TYPE_BATTERY_LOW){
+            notificationBuilder.setContentTitle("Battery Alert")
+                .setContentTitle("Plug charger in please😥")
+                .setContentText("Battery level is below $limit% 🥺")
+                .setSmallIcon(R.drawable.battery_warn)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+        }
+        // Create the battery is too hot notification
+        if( type == TYPE_BATTERY_HOT){
+            notificationBuilder.setContentTitle("Battery Alert")
+                .setContentText("Your battery temperature is too hot🥵! $temperature°C")
+                .setSmallIcon(R.drawable.battery_warn)
+                .setContentTitle("TOO HOT😱")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true)
+        }
+        if(type ==TYPE_BATTERY_LOW_ALARM){
+            notificationBuilder.setContentTitle("Battery Alert")
+                .setContentText("Charge your phone now! $limit% left")
+                .setContentTitle("BATTERY LOW😱")
+                .setSmallIcon(R.drawable.battery_warn)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true) // Set the notification to be automatically canceled
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)) // Set the notification sound
+
+        }
+        if(type ==TYPE_BATTERY_HOT_ALARM){
+            notificationBuilder.setContentTitle("Battery Alert")
+                .setContentTitle("TOO HOT😱")
+                .setContentText("Stop using your phone! $temperature°C")
+                .setSmallIcon(R.drawable.battery_warn)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setAutoCancel(true) // Set the notification to be automatically canceled
+                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)) // Set the notification sound
+        }
+        // Send the notification
+        notificationManager.notify(1, notificationBuilder.build())
     }
 }
